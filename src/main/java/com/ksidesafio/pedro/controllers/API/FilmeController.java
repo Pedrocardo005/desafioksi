50a4c15d392a62510a935a211596c3ded8ebd40c
Pedrocardo005 <pedro.cardoso@ssig.tech>
2023-11-02 17:15:17 -0300

    Obtem a lista de filmes corretamente
     - Cria endpoint que escolhe o filme selecionado

--- src/main/java/com/ksidesafio/pedro/controllers/API/FilmeController.java
+++ src/main/java/com/ksidesafio/pedro/controllers/API/FilmeController.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ksidesafio.pedro.models.Filme;
 import com.ksidesafio.pedro.services.FilmeService;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/filmes")
@@ -17,7 +20,14 @@ public class FilmeController {
     private FilmeService filmeService;
 
     @GetMapping
-    public ArrayList<Filme> getAll(){
+    public ArrayList<Filme> getAll() {
         return filmeService.getAll();
+    }
+
+    @PostMapping
+    public String choiceFilme(HttpServletRequest request) {
+        String texto = request.getParameter("favorite_filme");
+        filmeService.choiceFilme(texto);
+        return "Sucesso";
     }
 }
--- src/main/java/com/ksidesafio/pedro/controllers/template/FilmeTemplateController.java
+++ src/main/java/com/ksidesafio/pedro/controllers/template/FilmeTemplateController.java
@@ -8,19 +8,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ksidesafio.pedro.models.FavoriteFilme;
+import com.ksidesafio.pedro.models.Filme;
 import com.ksidesafio.pedro.services.FavoriteFilmeService;
+import com.ksidesafio.pedro.services.FilmeService;
 
 @Controller
 public class FilmeTemplateController {
 
     @Autowired
     private FavoriteFilmeService favoriteFilmeService;
+
+    @Autowired
+    private FilmeService filmeService;
     
     @GetMapping
     public String index(Model model) {
         ArrayList<FavoriteFilme> favoriteFilmes;
+        ArrayList<Filme> filmes;
         favoriteFilmes = favoriteFilmeService.findAll();
+        filmes = filmeService.getAll();
+
         model.addAttribute("favorite_filmes", favoriteFilmes);
+        model.addAttribute("filmes", filmes);
+
         return "index.html";
     }
 }
