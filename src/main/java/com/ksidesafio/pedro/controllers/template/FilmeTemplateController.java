d1fb7accbb49756f547a893d29b3eb9d3fd24e3a
Pedrocardo005 <pedro.cardoso@ssig.tech>
2023-11-02 16:04:22 -0300

    Add tabela ao front

--- src/main/java/com/ksidesafio/pedro/controllers/template/FilmeTemplateController.java
+++ src/main/java/com/ksidesafio/pedro/controllers/template/FilmeTemplateController.java
@@ -1,17 +1,26 @@
 package com.ksidesafio.pedro.controllers.template;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ksidesafio.pedro.models.FavoriteFilme;
+import com.ksidesafio.pedro.services.FavoriteFilmeService;
+
 @Controller
 public class FilmeTemplateController {
 
-    private String[] filmes = {"Os vingadores", "Capitão américa"};
+    @Autowired
+    private FavoriteFilmeService favoriteFilmeService;
     
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("filmes", filmes);
+        ArrayList<FavoriteFilme> favoriteFilmes;
+        favoriteFilmes = favoriteFilmeService.findAll();
+        model.addAttribute("favorite_filmes", favoriteFilmes);
         return "index.html";
     }
 }
