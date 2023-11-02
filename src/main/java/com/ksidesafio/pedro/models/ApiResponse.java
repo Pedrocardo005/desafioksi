095a2f5cff58f4e80a92d7b67d40380d5cd43f8d
Pedrocardo005 <pedro.cardoso@ssig.tech>
2023-11-02 17:14:04 -0300

    Cria o servico que cria o filme no banco e classe que ira obter os dados da api

---
+++ src/main/java/com/ksidesafio/pedro/models/ApiResponse.java
@@ -0,0 +1,9 @@
+package com.ksidesafio.pedro.models;
+
+public class ApiResponse {
+    public String Title;
+
+    public String Released;
+
+    public String Director;
+}
--- src/main/java/com/ksidesafio/pedro/services/FilmeService.java
+++ src/main/java/com/ksidesafio/pedro/services/FilmeService.java
@@ -1,12 +1,20 @@
 package com.ksidesafio.pedro.services;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.ksidesafio.pedro.models.ApiResponse;
 import com.ksidesafio.pedro.models.Filme;
 import com.ksidesafio.pedro.repository.FilmeRepository;
+
+import java.util.Date;
+import java.util.Locale;
 
 @Service
 public class FilmeService {
@@ -14,7 +22,38 @@ public class FilmeService {
     @Autowired
     private FilmeRepository filmeRepository;
 
+    private String urlBase = "https://www.omdbapi.com/?apikey=2536b146&i=";
+
     public ArrayList<Filme> getAll() {
         return (ArrayList<Filme>) filmeRepository.findAll();
+    }
+
+    public Filme create(Filme filme) {
+        return filmeRepository.save(filme);
+    }
+
+    public void choiceFilme(String id) {
+        Locale.setDefault(Locale.US);        
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<ApiResponse> response = restTemplate.getForEntity(urlBase + id, ApiResponse.class);
+    
+        if (response.getStatusCode() == HttpStatus.OK) {
+            ApiResponse responseBody = response.getBody();
+            Filme filme = new Filme();
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM yyyy");
+            try {
+                Date data = simpleDateFormat.parse(responseBody.Released);
+
+                filme.setNome(responseBody.Title);
+                filme.setAnoLancamento(data);
+                filme.setDiretor(responseBody.Director);
+                
+                create(filme);
+                System.out.println("Criou o filme");
+            } catch (Exception e) {
+                System.out.println("Erro gerado" + e.getMessage());
+            }
+        }
     }
 }
